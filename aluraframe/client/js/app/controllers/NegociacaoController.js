23fdc5654ae1ab7e0662d055cb99ed0ecde8c971
Raul-Pinheiro <raulmatheus.rm@gmail.com>
2020-12-29 11:52:36 -0300

    Segundo commit curso avançado JS - parte 2 (aula 1 e 2)

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,17 +1,31 @@
 class NegociacaoController {
     constructor() {
         let $ = document.querySelector.bind(document);
+
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
-        this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes();
-        this._negociacaoView = new NegociacaoView($('#negociacaoView'));
-        this._mensagem=new Mensagem();
+        this._inputValor = $('#valor'); 
 
+        this._listaNegociacoes = ProxyFactory.create(
+            new ListaNegociacoes(),
+            ['adiciona','esvazia'],
+            model=>{
+                this._negociacaoView.update(model)
+            }
+        );
+
+
+
+        this._negociacaoView = new NegociacaoView($('#negociacaoView'));
+        this._mensagem=ProxyFactory.create(
+            new Mensagem(),
+            ['texto'],
+            (model)=> this._mensagemView.update(model)
+        );
         this._mensagemView= new MensagemView($('#mensagemView'));
 
-        this._mensagemView.update(this._mensagem);
 
+       
         this._negociacaoView.update(this._listaNegociacoes);
 
 
@@ -38,13 +52,17 @@ class NegociacaoController {
 
         event.preventDefault();  
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._negociacaoView.update(this._listaNegociacoes);
         this._mensagem.texto='Negociação realizada com sucesso!';     
-        this._mensagemView.update(this._mensagem);        
+        //this._mensagemView.update(this._mensagem);        
         this._limpaFormulario();        
         console.log(this._listaNegociacoes.negociacao);
     }
 
+    apagaNegociacao(){
+        this._listaNegociacoes.esvazia();
+        this._mensagem.texto='Lista de Negociações Apagada com Sucesso';
+        //this._mensagemView.update(this._mensagem);
+    }
   
 
 }
